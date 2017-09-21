335aaf1f944b8a873fc615b7250dbdd1b16d6d34
Amruta <hail.radical@gmail.com>
2017-09-20 22:10:43 -0600

    modified file to use nltk library for further use

--- CountFrequency.py
+++ CountFrequency.py
@@ -1,25 +1,28 @@
 import operator
 import csv
+import nltk
 
 
 def give_freq_counts(filename, separator, column):
+    input_text = []
+    frequency = {}
     with open(filename, 'r') as file_obj:
-        frequency = {}
         for line in csv.reader(file_obj, delimiter=separator, skipinitialspace=True):
             if line:
                 input_word = line[column]
-                if input_word in frequency:
-                    count = frequency.get(input_word, 0)
-                    frequency[input_word] = count + 1
-                else:
-                    frequency[input_word] = 1
+                input_text.append(input_word)
 
-    sorted_x = sorted(frequency.items(), key=operator.itemgetter(1), reverse=True)
-    print(sorted_x)
-    return sorted_x
+    input_vocab = set(input_text)
+    freq_dist = nltk.FreqDist(input_text)
+    for word in input_vocab:
+        frequency[word] = freq_dist[word]
+    sorted_freq = sorted(frequency.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_freq
+
+
 
 '''
 if __name__ == "__main__":
-    output_file = give_freq_counts(r'Training_Berp.txt', '\t', 1)
-    print("success")
+    output_file = give_freq_counts(r'Training_Berp.txt', '\t', 2)
+    print(output_file)
 '''
